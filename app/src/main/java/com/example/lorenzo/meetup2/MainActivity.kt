ecdbfef41600702622667ef803ef97a45d21504b
westune <moiseslorenzo13@gmail.com>
2018-11-14 12:42:16 -0800

    More UI and broken Sign In

--- app/src/main/java/com/example/lorenzo/meetup2/MainActivity.kt
+++ app/src/main/java/com/example/lorenzo/meetup2/MainActivity.kt
@@ -1,14 +1,28 @@
 package com.example.lorenzo.meetup2
 
+import android.content.Intent
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import android.support.design.widget.BottomNavigationView
-import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.MenuItem
+import com.google.android.gms.common.api.GoogleApiClient
 import kotlinx.android.synthetic.main.activity_main.*
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.common.ConnectionResult
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedListener {
 
-    val manager = supportFragmentManager
+    //Fragment Manager
+    private val manager = supportFragmentManager
+    //Firebase Auth
+    private var mAuth:FirebaseAuth? = null
+    private var mUser:FirebaseUser? = null
+    private var mUserName:String = ""
+    private var mPhotoUrl:String = ""
+    private var mGoogleApiClient:GoogleApiClient? = null
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
@@ -27,11 +41,53 @@ class MainActivity : AppCompatActivity() {
         false
     }
 
+    override fun onConnectionFailed(p0: ConnectionResult) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mAuth = FirebaseAuth.getInstance()
+        mUser = mAuth!!.currentUser
         setContentView(R.layout.activity_main)
+
+        /*
+        if(mUser == null){
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+            finish()
+            return
+        } else {
+            mUserName = mUser!!.displayName!!
+            if(mUser!!.photoUrl != null){
+                mPhotoUrl = mUser!!.photoUrl.toString()
+            }
+        }
+        */
+
+
         showBuyList()
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        /*
+        when(item.itemId){
+            R.id.sign_out_menu ->{
+                mAuth!!.signOut()
+                Auth.GoogleSignInApi.signOut(mGoogleApiClient)
+                mUserName = ""
+                val intent = Intent(this, SignInActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+            else -> {
+                Log.d(TAG, "yes")
+                return super.onOptionsItemSelected(item)
+            }
+        }
+        */
+        return super.onOptionsItemSelected(item)
     }
 
     private fun showBuyList(){
--- app/src/main/java/com/example/lorenzo/meetup2/SignInActivity.kt
+++ app/src/main/java/com/example/lorenzo/meetup2/SignInActivity.kt
@@ -1,19 +1,101 @@
 package com.example.lorenzo.meetup2
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import com.google.firebase.FirebaseError
+import com.google.firebase.auth.FirebaseAuth
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInResult
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.GoogleApi
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.GoogleAuthCredential
+import com.google.firebase.auth.GoogleAuthProvider
 
-class SignInActivity: AppCompatActivity(){
+class SignInActivity: AppCompatActivity(), GoogleApiClient.OnConnectionFailedListener, View.OnClickListener{
 
     private val TAG = "Sign In Activity"
     private val RC_SIGN_IN = 9001
+    private var mAuth:FirebaseAuth? = null
+    private var mGoogleApiClient:GoogleApiClient? = null
+    private var mSignInButton:SignInButton? = null
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.acitivity_sign_in)
+
+
+        //Sign In Button Listener
+        mSignInButton = findViewById(R.id.sign_in_button)
+        mSignInButton!!.setOnClickListener(this)
+
+        //Configure Google Sign In
+        val googleSignInOptions:GoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(R.string.default_web_client_id.toString())
+                .requestEmail()
+                .build()
+        mGoogleApiClient = GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
+                .build()
+        mAuth = FirebaseAuth.getInstance()
+
     }
 
+    private fun signIn(){
+        val signInIntent: Intent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient)
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
 
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.sign_in_button->{
+                signIn()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == RC_SIGN_IN){
+            var result:GoogleSignInResult = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if(result.isSuccess){
+                var account:GoogleSignInAccount? = result.signInAccount
+                firebaseAuthWithGoogle(account!!)
+            } else{
+                Log.d(TAG, "Google Sign in failed")
+            }
+        }
+    }
+
+    override fun onConnectionFailed(result: ConnectionResult) {
+        Log.d(TAG, "onConnectionFailed: " + result)
+    }
+
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount){
+        var cred: AuthCredential = GoogleAuthProvider.getCredential(account.idToken, null)
+        mAuth!!.signInWithCredential(cred).addOnCompleteListener(this){
+            Log.d(TAG, "SignInWithCredential:onComplete: " + it.isSuccessful)
+            if(!it.isSuccessful){
+                Log.w(TAG, "signInWithCredentail", it.exception)
+                Toast.makeText(this, "Authentication Failed", Toast.LENGTH_SHORT)
+            }else{
+                var intent:Intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+        }
+    }
 
 }
